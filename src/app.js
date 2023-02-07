c2727c841dad75f7dbe122b655453f76f9da020d
Angie <angelicareutersward@hotmail.com>
2023-02-07 14:27:56 +0100

    app.use i app.js tillagt för alla routes

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app.use("/helloWorld", (request, response) => {
   return response.send("Hello World!");
 });
 
+app.use("/api/v1/restaurants" /* /... = see Router => */, restaurantRoutes);
+app.use("/api/v1/reviews" /* /... = see Router => */, reviewRoutes);
+app.use("/api/v1/users" /* /... = see Router => */, userRoutes);
+
 const port = process.env.PORT || 4000;
 
 async function run() {
