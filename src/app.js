42dc559ab5c2fac31c3ee2f3706630be5737374c
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-02-07 13:08:58 +0100

    app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use("/helloWorld", (request, response) => {
+  return response.send("Hello World!");
+});
+
+const port = process.env.PORT || 5000;
+
+async function run() {
+  app.listen(5000, () => {
+    console.log("Server running");
+  });
+}
+run();
